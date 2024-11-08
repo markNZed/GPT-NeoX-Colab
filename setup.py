a28926d6d5d66e8c86ce1d36dfda4faf143c942f
Mark Hampton <mark.hampton@ieee.org>
2024-11-08 12:01:24 +0000

    requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+# Read requirements from requirements.txt
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 setup(
     name="GPTNeoXColab",
     version="0.1",
     package_dir={"": "src"},  # Specify src as the root directory for packages
     packages=find_packages(where="src"),  # Search for packages in src
+    install_requires=required,
 )
