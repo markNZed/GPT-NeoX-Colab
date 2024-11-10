8a65975dd26a1858e4ea2f5b7ee26b791cac21fe
Your Name <you@example.com>
2024-11-10 09:05:37 +0000

    requirements_colab

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 # Read requirements from requirements.txt
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
+#with open("requirements.txt") as f:
+#    required = f.read().splitlines()
 
 setup(
     name="GPTNeoXColab",
     version="0.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=required,
+    #install_requires=required,
 )
